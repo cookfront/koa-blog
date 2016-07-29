035d3f9450da6cb2edc0ba3af7dad83a07e1600e
cookfront <cookfront@gmail.com>
2016-07-29 21:23:29 +0800

    blog.sql commit

--- blog.sql
+++ blog.sql
@@ -1,12 +1,34 @@
+-- 文章表
+CREATE TABLE articles (
+  `aid` int(10) unsigned NOT NULL auto_increment,
+  `title` varchar(200) default NULL,
+  `slug` varchar(200) default NULL,
+  `created_at` int(10) unsigned default '0',
+  `updated_at` int(10) unsigned default '0',
+  `text` text,
+  `order` int(10) unsigned default '0',
+  `authorId` int(10) unsigned default '0',
+  `type` varchar(16) default 'post',
+  `status` varchar(16) default 'publish',
+  `password` varchar(32) default NULL,
+  `commentsNum` int(10) unsigned default '0',
+  `allowComment` char(1) default '0',
+  `allowFeed` char(1) default '0',
+  PRIMARY KEY  (`cid`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `created` (`created`)
+);
+
 -- 文章和文章元信息表关联表
 CREATE TABLE articles_metas (
-  `aid` int(11),
-  `mid` int(11)
+  `aid` int(10) unsigned NOT NULL,
+  `mid` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`aid`,`mid`)
 );
 
 -- 文章元信息表
 CREATE TABLE metas (
-  `cid` int(11) unsigned NOT NULL auto_increment,
+  `cid` int(10) unsigned NOT NULL auto_increment,
   `name` varchar(200) default NULL,
   `slug` varchar(200) default NULL,
   `type` varchar(32) NOT NULL,
@@ -31,4 +53,24 @@ CREATE TABLE users (
   PRIMARY KEY  (`uid`),
   UNIQUE KEY `name` (`name`),
   UNIQUE KEY `mail` (`mail`)
+);
+
+CREATE TABLE `comments` (
+  `coid` int(10) unsigned NOT NULL auto_increment,
+  `cid` int(10) unsigned default '0',
+  `created_at` int(10) unsigned default '0',
+  `author` varchar(200) default NULL,
+  `authorId` int(10) unsigned default '0',
+  `ownerId` int(10) unsigned default '0',
+  `email` varchar(200) default NULL,
+  `url` varchar(200) default NULL,
+  `ip` varchar(64) default NULL,
+  `agent` varchar(200) default NULL,
+  `text` text,
+  `type` varchar(16) default 'comment',
+  `status` varchar(16) default 'approved',
+  `parent` int(10) unsigned default '0',
+  PRIMARY KEY  (`coid`),
+  KEY `cid` (`cid`),
+  KEY `created` (`created`)
 );
